d677c5d04ae2350ef30b6ad5c1e20998b7effccf
mnPanic <panicmanu@gmail.com>
2022-09-30 19:25:35 -0300

    fin

--- taller-sql-2022-1c/ejs-taller-2022-2c.sql
+++ taller-sql-2022-1c/ejs-taller-2022-2c.sql
@@ -1,6 +1,10 @@
--- Ejs taller 2022 2c --
+-- Ejs taller 2022 2c, Turno 2 --
+
+-- Manuel Panichelli, L.U 72/18
 -- Orden 16
--- Ej 1 - Obtener la cantidad de ventas por año discriminando por país
+
+-- Ej 1 --
+-- Obtener la cantidad de ventas por año discriminando por país
 
 SELECT
     date_part('year', i.invoice_date) as year,
@@ -8,12 +12,20 @@ SELECT
     COUNT(i.invoice_id) as num_sales
 FROM invoice i
 GROUP BY year, i.billing_country
-ORDER BY year, i.billing_country
+ORDER BY year, num_sales -- no está pedido por el enunciado pero me gusta más como queda con esto
 
--- Ej 2 - Cuáles son lo artistas que tienen más álbumes
+-- Ej 2 --
+-- Cuáles son lo artistas que tienen más álbumes
 
 -- Estrategia: quiero contar la cantidad de álbumes que tiene cada artista, y luego
--- me quedo con los que esa cantidad es mayor a la del resto de todos los artistas.
+-- me quedo con los que tienen cantidad mayor a la de todos los otros artistas.
+
+-- Para este y otros ejercicios, si me hubieran pedido dar la cantidad de álbumes por artista
+-- debería haber hecho un left join para contemplar a los artistas que no tienen álbumes (y que de 0).
+-- Como sé que hay artistas que tienen álbumes, el resultado de la query no cambia poniendo inner o
+-- left join. Si quisiera que la query sea agnóstica a los datos y más correcta, debería haber
+-- puesto left join, pero para simplificar lo dejé con inner.
+
 SELECT a.artist_id, a.name, COUNT(al.album_id) AS num_albums
 FROM artist a
     JOIN album al ON al.artist_id = a.artist_id
@@ -25,11 +37,13 @@ HAVING COUNT(al.album_id) >= ALL (
     GROUP BY a.artist_id
 )
 
--- Ej 3 - Obtener el precio del track más caro para cada género indicando el álbum al que pertenece
+-- Ej 3 --
+-- Obtener el precio del track más caro para cada género indicando el álbum al que pertenece
 
--- Duda: está bien devolver más de un álbum?
+-- Agrego DISTINCT porque puede haber más de un track que tenga el precio máximo,
+-- y que más de uno de ellos pertenezca al mismo álbum.
 
-SELECT
+SELECT DISTINCT
     g.genre_id,
     g.name,
     t.unit_price as max_track_price,
@@ -41,14 +55,15 @@ WHERE t.unit_price = (
     SELECT MAX(t2.unit_price)
     FROM track t2
     WHERE t2.genre_id = g.genre_id
+)
 
-
--- Ej 4: Cuales son los artistas que más tracks vendieron
+-- Ej 4 --
+-- Cuales son los artistas que más tracks vendieron
 
 -- Un track está vendido si aparece en un invoice line, entonces la cantidad de ventas de un track
--- está determinada por la cantidad de veces que aparece en un invoice line.
+-- está determinada por la cantidad de invoice lines con los que se relaciona.
 
-SELECT ar.artist_id, ar.name, COUNT(il.invoice_line_id) AS sold_tracks
+SELECT ar.artist_id, ar.name, COUNT(il.invoice_line_id) AS tracks_sold
 FROM artist ar
     JOIN album al ON al.artist_id = ar.artist_id
     JOIN track t ON t.album_id = al.album_id
@@ -63,13 +78,11 @@ HAVING COUNT(il.invoice_line_id) >= ALL (
     GROUP BY ar.artist_id
 )
 
--- Ej 5 - Listar playlists cuyos tracks tengan al menos dos álbumes
+-- Ej 5 --
+-- Listar playlists cuyos tracks tengan al menos dos álbumes
 
--- Está bien partir de playlist_track en vez de playlist y hacer inner en vez de left join
--- para hacer el select dado que si la playlist no tiene tracks, entonces no van a poder ser
--- de al menos dos álbumes.
---
 -- Tengo que hacer count distinct porque una playlist puede tener varios tracks del mismo álbum
+
 SELECT pt.playlist_id, p.name, COUNT(distinct al.album_id) AS num_albums
 FROM playlist_track pt
     JOIN track t ON t.track_id = pt.track_id
@@ -79,14 +92,14 @@ GROUP BY pt.playlist_id, p.name
 HAVING COUNT(distinct al.album_id) >= 2
 ORDER BY num_albums DESC
 
--- Ej 6 - Realizar una consulta correlacionada que devuelva, si es que lo hubiera, todas las 
+-- Ej 6 --
+-- Realizar una consulta correlacionada que devuelva, si es que lo hubiera, todas las 
 -- playlists que tengan algún track cuyo álbum incluya la palabra "Live"
 
-WITH foo AS (
 SELECT p.playlist_id, p.name
 FROM playlist p
 WHERE EXISTS (
-    -- Existe un track cuyo álbum incluye la palabra "Live"
+    -- Existe un track de la playlist cuyo álbum incluye la palabra "Live"
     SELECT t.track_id
     FROM track t
         JOIN playlist_track pt ON pt.track_id = t.track_id
@@ -95,11 +108,3 @@ WHERE EXISTS (
         al.title LIKE '%Live%' AND
         p.playlist_id = pt.playlist_id -- Correlacionada porque está usando p que es de la outer query
 )
-)
-
-SELECT p.playlist_id, p.name, t.name, al.title
-FROM foo f
-    JOIN playlist p ON p.playlist_id = f.playlist_id
-    JOIN playlist_track pt ON pt.playlist_id = p.playlist_id
-    JOIN track t ON t.track_id = pt.track_id
-    JOIN album al ON t.album_id = t.album_id
